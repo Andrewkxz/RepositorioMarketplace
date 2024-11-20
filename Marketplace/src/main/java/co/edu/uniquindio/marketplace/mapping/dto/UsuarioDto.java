4b510f0c9aa14a2ddab5afa6f33c82560276c72b
yeri966 <142251189+yeri966@users.noreply.github.com>
2024-11-19 23:56:10 -0500

    Update UsuarioDto.java

--- Marketplace/src/main/java/co/edu/uniquindio/marketplace/mapping/dto/UsuarioDto.java
+++ Marketplace/src/main/java/co/edu/uniquindio/marketplace/mapping/dto/UsuarioDto.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.marketplace.mapping.dto;
 
+/**
+ * La clase UsuarioDto es un "Record" que representa un objeto de transferencia de datos (DTO).
+ * En este caso, se utiliza para transferir la información relacionada con un usuario.
+ * Este DTO contiene el nombre de usuario y la contraseña del usuario.
+ */
 public record UsuarioDto(
         String usuario,
         String contrasenia) {
-
 }
